a600029ebfb8fd3af882dbea604e2707198660a0
Chunhao Hsu <cyan.hch@gmail.com>
2023-04-21 23:49:53 -0600

    update my branch

--- droplet.cpp
+++ droplet.cpp
@@ -4,7 +4,7 @@
 droplet::droplet(QObject *parent)
     : QObject{parent}
 {
-    int random_number = rand() % 900;
+    int random_number = arc4random() % 900;
     this -> setPixmap((QPixmap(":/new/prefix1/water.gif")).scaled(20,40));
     this -> setPos(random_number, 110);
     QTimer *timer_drop = new QTimer(this);
@@ -19,7 +19,7 @@ void droplet::move_droplet() {
     yPos += 10;
     this -> setPos(x(), yPos);
 
-    if (collidingItems().size() > 0 | yPos>450) {
+    if (collidingItems().size() > 0 || yPos>450) {
         this->scene()->removeItem(this);
         delete this;
     }
