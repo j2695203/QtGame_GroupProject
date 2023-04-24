5fdc2a9c3ef4776e9106923444acffceac193279
Howard Tung <u1422283@umail.utah.edu>
2023-04-24 08:36:34 -0600

    Adding loading when open profile page

--- profile.cpp
+++ profile.cpp
@@ -13,6 +13,8 @@ profile::profile(User *user)
         QUrl url(user->imageURL);
         QNetworkAccessManager *manager = new QNetworkAccessManager(this);
         QNetworkReply *reply = manager->get(QNetworkRequest(url));
+        pic = new QLabel("Loading...");
+        mainLayout->addWidget(pic);
         connect(reply, &QNetworkReply::finished, [=]() {
             if (reply->error() == QNetworkReply::NoError) {
                 QByteArray data = reply->readAll();
@@ -21,28 +23,27 @@ profile::profile(User *user)
                 pixmap.loadFromData(data);
 
                 pic->setPixmap(pixmap.scaled(100, 100));
+                firstName->setText("First name: " + user->firstName);
+                lastName->setText("Last name: " + user->lastName);
+                birthday->setText("Birthday: " + user->birthday);
+
+                // set style
+                mainLayout->setContentsMargins(60, 60, 60, 60);
+
+                // add widgets to layout
+
+                mainLayout->addWidget(title, 0, Qt::AlignCenter);
+                mainLayout->addWidget(pic, 0, Qt::AlignCenter);
+                mainLayout->addWidget(username);
+                mainLayout->addWidget(birthday);
+                mainLayout->addWidget(firstName);
+                mainLayout->addWidget(lastName);
+
+                // set layout to window
             }
             reply->deleteLater();
             manager->deleteLater();
         });
-
-        firstName->setText("First name: " + user->firstName);
-        lastName->setText("Last name: " + user->lastName);
-        birthday->setText("Birthday: " + user->birthday);
-
-        // set style
-        mainLayout->setContentsMargins(60, 60, 60, 60);
-
-        // add widgets to layout
-
-        mainLayout->addWidget(title, 0, Qt::AlignCenter);
-        mainLayout->addWidget(pic, 0, Qt::AlignCenter);
-        mainLayout->addWidget(username);
-        mainLayout->addWidget(birthday);
-        mainLayout->addWidget(firstName);
-        mainLayout->addWidget(lastName);
-
-        // set layout to window
 
     } else {
         username->setText("Username: Guest");
